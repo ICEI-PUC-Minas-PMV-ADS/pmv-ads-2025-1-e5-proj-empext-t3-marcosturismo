0ced1c7a1a5a9a981e518b086f577c2247ebac8d
Maria de Lourdes Monteiro da Silva <130480347+LourdesMonteiro@users.noreply.github.com>
2025-03-24 16:03:05 +0000

    Add files via upload

---
+++ codigo-fonte/frontend/marcos-turismo/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [SidebarComponent, RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  isSidebarActive = true;
+
+  onToggleSidebar(active: boolean) {
+    this.isSidebarActive = active;
+  }
+}
