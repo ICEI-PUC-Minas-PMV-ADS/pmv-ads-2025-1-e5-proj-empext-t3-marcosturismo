6aa9cab21e618a548bea49ba508afa03f16b9ba7
Israel <israel.silva.1443145@sga.pucminas.br>
2025-03-24 16:14:43 -0300

    Tela de login

--- codigo-fonte/frontend/marcos-turismo/src/app/app.routes.ts
+++ codigo-fonte/frontend/marcos-turismo/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import path from 'node:path';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   { path: 'usuario', component: UsuarioComponent },
+  { path: 'login', component: LoginComponent},
   { path: '', redirectTo: '/usuario', pathMatch: 'full' } // opcional
 ];
---
+++ codigo-fonte/frontend/marcos-turismo/src/app/components/login/login.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient, HttpErrorResponse, HttpClientModule } from '@angular/common/http';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      login: ['', [Validators.required, Validators.email]],
+      senha: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      alert('Por favor, preencha todos os campos corretamente.');
+      return;
+    }
+
+    const email = this.loginForm.get('login')?.value;
+    const senha = this.loginForm.get('senha')?.value;
+
+    this.http.post<any>('http://localhost:8080/auth/login', { email, senha }, {
+      withCredentials: true
+    }).subscribe({
+      next: (response: { token: string; }) => {
+        console.log('Resposta do servidor:', response);
+
+        if (response.token) {
+          localStorage.setItem('token', response.token);
+          alert('Login bem-sucedido!');
+          this.router.navigate(['/dashboard']);
+        } else {
+          alert('Erro: Token não recebido.');
+        }
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error('Erro ao conectar ao servidor:', error);
+        alert(error.error?.message || 'Erro ao fazer login. Tente novamente.');
+      }
+    });
+  }
+}
