eb84c92e6ef621dd1adf44e724deb0967bc21b3c
Israel <israel.silva.1443145@sga.pucminas.br>
2025-03-25 14:04:22 -0300

    Tela de Avaliação

--- codigo-fonte/frontend/marcos-turismo/src/app/app.routes.ts
+++ codigo-fonte/frontend/marcos-turismo/src/app/app.routes.ts
@@ -4,10 +4,11 @@ import { UsuarioComponent } from './components/usuario/usuario.component';
 
 import path from 'node:path';
 import { LoginComponent } from './components/login/login.component';
+import { AvaliacaoComponent } from './components/avaliacao/avaliacao.component';
 
 export const routes: Routes = [
   { path: 'usuario', component: UsuarioComponent },
   { path: 'login', component: LoginComponent},
-
+  { path: 'avaliacao', component: AvaliacaoComponent},
   { path: '', redirectTo: '/usuario', pathMatch: 'full' } // opcional
 ];
