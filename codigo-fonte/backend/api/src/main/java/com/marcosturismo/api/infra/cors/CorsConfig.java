8c101e7f781e447a017ab3902daed641fe483af3
davihaniel <davihani3l@gmail.com>
2025-03-25 19:18:12 -0300

    Erro de CORS

--- codigo-fonte/backend/api/src/main/java/com/marcosturismo/api/infra/cors/CorsConfig.java
+++ codigo-fonte/backend/api/src/main/java/com/marcosturismo/api/infra/cors/CorsConfig.java
@@ -1,17 +1,26 @@
 package com.marcosturismo.api.infra.cors;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                ///  Tem que trocar o localhost
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true); // Allow credentials (cookies, authorization headers)
+        configuration.addAllowedOrigin("http://127.0.0.1:5500"); // Allow your frontend origin
+        configuration.addAllowedOrigin("http://localhost:4200");
+
+        configuration.addAllowedMethod("*"); // Allow all HTTP methods
+        configuration.addAllowedHeader("*"); // Allow all headers
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Apply to all paths
+        return source;
     }
 }
