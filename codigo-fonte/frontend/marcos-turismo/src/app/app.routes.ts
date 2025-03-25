be8600b916dfbe075c55b49732004839733ed68f
Jefferson Freitas <jefferson.freitas.10@outlook.com>
2025-03-25 20:52:03 -0300

    Tela de Cliente

--- codigo-fonte/frontend/marcos-turismo/src/app/app.routes.ts
+++ codigo-fonte/frontend/marcos-turismo/src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { UsuarioComponent } from './components/usuario/usuario.component';
 import path from 'node:path';
 import { LoginComponent } from './components/login/login.component';
 import { AvaliacaoComponent } from './components/avaliacao/avaliacao.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
 
 export const routes: Routes = [
   { path: 'usuario', component: UsuarioComponent },
   { path: 'login', component: LoginComponent},
   { path: 'avaliacao', component: AvaliacaoComponent},
+  { path: 'cliente', component: ClienteComponent},
   { path: '', redirectTo: '/usuario', pathMatch: 'full' } // opcional
 ];
