628f2e6984ba405a0e544d617302bcfad9b1f394
Leonardo JPaula <129237541+Leo-JdP@users.noreply.github.com>
2025-03-26 17:59:40 -0300

    Tela de Excursão

--- codigo-fonte/frontend/marcos-turismo/src/app/app.routes.ts
+++ codigo-fonte/frontend/marcos-turismo/src/app/app.routes.ts
@@ -6,21 +6,24 @@ import path from 'node:path';
 import { LoginComponent } from './components/login/login.component';
 import { AvaliacaoComponent } from './components/avaliacao/avaliacao.component';
 import { ClienteComponent } from './components/cliente/cliente.component';
-<<<<<<< Updated upstream
+import { ExcursoesComponent } from './components/excursoes/excursoes.component';
+
+
 import { FrotaComponent } from './components/frota/frota.component';
-=======
+
 import { ViagensComponent } from './components/viagens/viagens.component';
->>>>>>> Stashed changes
+
 
 export const routes: Routes = [
   { path: 'usuario', component: UsuarioComponent },
   { path: 'login', component: LoginComponent},
   { path: 'avaliacao', component: AvaliacaoComponent},
   { path: 'cliente', component: ClienteComponent},
-<<<<<<< Updated upstream
+  { path: 'excursoes', component: ExcursoesComponent},
+
   { path: 'frota', component: FrotaComponent},
-=======
+
   { path: 'viagens', component: ViagensComponent},
->>>>>>> Stashed changes
+
   { path: '', redirectTo: '/usuario', pathMatch: 'full' } // opcional
 ];
